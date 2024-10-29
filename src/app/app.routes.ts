40a0cf1a95e3bd738df81a179ab182c969efbf22
Felipe Quérette <felipe.querette@gmail.com>
2024-10-29 18:32:22 -0300

    update sidemenu: add profile-based links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { RecordsDetailComponent } from './components/records/records-detail/reco
 import { authGuard } from './guards/auth.guard';
 import { loginGuard } from './guards/login.guard';
 import { perfilGuard } from './guards/perfil.guard';
+import { UsersComponent } from './components/users/users.component';
 
 export const routes: Routes = [
     {
@@ -72,5 +73,10 @@ export const routes: Routes = [
             { path: ":id", component: ExamComponent },
         ],
         canActivate: [authGuard],
+    },
+    {
+        path: "users-list",
+        component: UsersComponent,
+        // canActivate: [authGuard, perfilGuard],
     },
 ];
