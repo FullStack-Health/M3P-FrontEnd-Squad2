ad4e9e66118bff28f38d0987dc3e894bd1032b31
Felipe Quérette <felipe.querette@gmail.com>
2024-10-30 00:12:04 -0300

    update admin guard

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,10 +1,13 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const adminGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
   const serialized: string | null = localStorage.getItem("loggedUser");
   if (!serialized)  return false;
   const user = JSON.parse(serialized);
 
   if (user.perfil == "ADMIN") return true;
+  router.navigate(["home"]);
   return false;
 };
